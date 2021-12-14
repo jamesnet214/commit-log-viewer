bfaab18afc72595286bd1c3cc4e5cdd3893f56e0
james.lee <james.lee@devncore.org>
2021-12-15 00:34:14 +0900

    Add Design icon

---
+++ source/DevNcore.UI.Design/Controls/Icon.cs
@@ -0,0 +1,8 @@
+﻿using DevNcore.WPF.Controls;
+
+namespace DevNcore.UI.Design.Controls
+{
+    public class Icon : NcoreIcon
+    {
+    }
+}
--- source/DevNcore.WPF/Controls/NcoreIcon.cs
+++ source/DevNcore.WPF/Controls/NcoreIcon.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 
 namespace DevNcore.WPF.Controls
 {
-    internal class NcoreIcon : Control
+    public class NcoreIcon : Control
     {
         public static readonly DependencyProperty DataProperty =
             DependencyProperty.Register(
