ebc0f3a6c151857518f721306eb4f6f6a893d9b5
devncore-elena <elena.kim@devncore.org>
2021-12-15 17:36:54 +0900

    Update NcoreIcon

--- source/DevNcore.WPF/Controls/NcoreIcon.cs
+++ source/DevNcore.WPF/Controls/NcoreIcon.cs
@@ -6,96 +6,54 @@ namespace DevNcore.WPF.Controls
 {
     public class NcoreIcon : Control
     {
-        public static readonly DependencyProperty DataProperty =
-            DependencyProperty.Register(
-                "Data", 
-                typeof(Geometry), 
-                typeof(NcoreIcon), 
-                new PropertyMetadata(null));
-
-        public static readonly DependencyProperty FillProperty =
-            DependencyProperty.Register(
-                "Fill",
-                typeof(Brush), 
-                typeof(NcoreIcon), 
-                new PropertyMetadata(null));
-
-        public static readonly DependencyProperty StrokeProperty =
-            DependencyProperty.Register(
-                "Stroke", 
-                typeof(Brush), 
-                typeof(NcoreIcon), 
-                new PropertyMetadata(null));
-
-        public static readonly DependencyProperty StrokeThicknessProperty = 
-            DependencyProperty.Register(
-                "StrokeThickness", 
-                typeof(Thickness), 
-                typeof(NcoreIcon),
-                new PropertyMetadata(new Thickness(0, 0, 0, 0)));
-
-        public static readonly DependencyProperty CornerRadiusProperty = 
-            DependencyProperty.Register(
-                "CornerRadius", 
-                typeof(CornerRadius), 
-                typeof(NcoreIcon), 
-                new PropertyMetadata(new CornerRadius(0, 0, 0, 0)));
-
-        public static readonly DependencyProperty GeometryWidthProperty = 
-            DependencyProperty.Register(
-                "GeometryWidth", 
-                typeof(double), 
-                typeof(NcoreIcon),
-                new PropertyMetadata(0.0));
-
-        public static readonly DependencyProperty GeometryHeightProperty = 
-            DependencyProperty.Register(
-                "GeometryHeight", 
-                typeof(double), 
-                typeof(NcoreIcon), 
-                new PropertyMetadata(0.0));
+        public static readonly DependencyProperty DataProperty = DependencyProperty.Register("Data", typeof(Geometry), typeof(NcoreIcon), new PropertyMetadata(null));
+        public static readonly DependencyProperty FillProperty = DependencyProperty.Register("Fill", typeof(Brush), typeof(NcoreIcon), new PropertyMetadata(null));
+        public static readonly DependencyProperty StrokeProperty = DependencyProperty.Register("Stroke", typeof(Brush), typeof(NcoreIcon), new PropertyMetadata(null));
+        public static readonly DependencyProperty StrokeThicknessProperty = DependencyProperty.Register("StrokeThickness", typeof(Thickness), typeof(NcoreIcon), new PropertyMetadata(new Thickness(0, 0, 0, 0)));
+        public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.Register("CornerRadius", typeof(CornerRadius), typeof(NcoreIcon), new PropertyMetadata(new CornerRadius(0, 0, 0, 0)));
+        public static readonly DependencyProperty GeometryWidthProperty = DependencyProperty.Register("GeometryWidth", typeof(double), typeof(NcoreIcon), new PropertyMetadata(0.0));
+        public static readonly DependencyProperty GeometryHeightProperty = DependencyProperty.Register("GeometryHeight", typeof(double), typeof(NcoreIcon), new PropertyMetadata(0.0));
 
         public Geometry Data
         {
-            get { return (Geometry)this.GetValue(DataProperty); }
-            set { this.SetValue(DataProperty, value); }
+            get => (Geometry)GetValue(DataProperty);
+            set => SetValue(DataProperty, value);
         }
 
         public Brush Fill
         {
-            get { return (Brush)this.GetValue(FillProperty); }
-            set { this.SetValue(FillProperty, value); }
+            get => (Brush)GetValue(FillProperty);
+            set => SetValue(FillProperty, value);
         }
-
 
         public Brush Stroke
         {
-            get { return (Brush)this.GetValue(StrokeProperty); }
-            set { this.SetValue(StrokeProperty, value); }
+            get => (Brush)GetValue(StrokeProperty);
+            set => SetValue(StrokeProperty, value);
         }
 
         public Thickness StrokeThickness
         {
-            get { return (Thickness)this.GetValue(StrokeThicknessProperty); }
-            set { this.SetValue(StrokeThicknessProperty, value); }
+            get => (Thickness)GetValue(StrokeThicknessProperty);
+            set => SetValue(StrokeThicknessProperty, value);
         }
 
         public CornerRadius CornerRadius
         {
-            get { return (CornerRadius)this.GetValue(CornerRadiusProperty); }
-            set { this.SetValue(CornerRadiusProperty, value); }
+            get => (CornerRadius)GetValue(CornerRadiusProperty);
+            set => SetValue(CornerRadiusProperty, value);
         }
 
         public double GeometryWidth
         {
-            get { return (double)this.GetValue(GeometryWidthProperty); }
-            set { this.SetValue(GeometryWidthProperty, value); }
+            get => (double)GetValue(GeometryWidthProperty);
+            set => SetValue(GeometryWidthProperty, value);
         }
 
         public double GeometryHeight
         {
-            get { return (double)this.GetValue(GeometryHeightProperty); }
-            set { this.SetValue(GeometryHeightProperty, value); }
+            get => (double)GetValue(GeometryHeightProperty);
+            set => SetValue(GeometryHeightProperty, value);
         }
     }
 }
