79104503783ea218267a542c98d22d17a82f8f8d
james.lee <james.lee@devncore.org>
2021-12-17 04:10:09 +0900

    Update References

--- source/CommitLogView/App.cs
+++ source/CommitLogView/App.cs
@@ -9,7 +9,7 @@ namespace CommitLogView
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
-            Resources.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("/CommitLogView;component/Themes/Units/ScrollViewer.xaml", UriKind.RelativeOrAbsolute) });
+            Resources.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("/CommitLogView;component/Themes/Units/WindowScrollViewer.xaml", UriKind.RelativeOrAbsolute) });
             Resources.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("/DevNcore.UI.Design.Geometry;component/Themes/Packages.xaml", UriKind.RelativeOrAbsolute) });
             var win = new MainWindow();
 
