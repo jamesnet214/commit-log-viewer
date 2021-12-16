efcf92a7be68fb21c60c1cd7c8049301ba0b58bf
james.lee <james.lee@devncore.org>
2021-12-17 06:18:20 +0900

    Detached CommitCard

---
+++ source/CommitLogView/UI/Units/CommitFileListItemCard.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CommitLogView.UI.Units
+{
+    internal class CommitFileListItemCard : ContentControl
+    {
+        #region DefaultStyleKey
+
+        static CommitFileListItemCard()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CommitFileListItemCard), new FrameworkPropertyMetadata(typeof(CommitFileListItemCard)));
+        }
+        #endregion
+    }
+}
