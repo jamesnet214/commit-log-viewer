2b3b9f39cccdcfacbb2aa40273b00743f475fd5d
james.lee <james.lee@devncore.org>
2021-12-17 06:28:18 +0900

    Detached Commits

---
+++ source/CommitLogView/UI/Units/CommitListBox.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace CommitLogView.UI.Units
+{
+    public class CommitListBox : RepoFileListBox
+    {
+        #region DefaultStyleKey
+
+        static CommitListBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CommitListBox), new FrameworkPropertyMetadata(typeof(CommitListBox)));
+        }
+        #endregion
+
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new CommitListItem();
+        }
+    }
+}
---
+++ source/CommitLogView/UI/Units/CommitListItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CommitLogView.UI.Units
+{
+    internal class CommitListItem : ListBoxItem
+    {
+        #region DefaultStyleKey
+
+        static CommitListItem()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CommitListItem), new FrameworkPropertyMetadata(typeof(CommitListItem)));
+        }
+        #endregion
+    }
+}
