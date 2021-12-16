30616cef7aa4979a6400ba020b444179d07b6411
james.lee <james.lee@devncore.org>
2021-12-17 03:35:56 +0900

    Attached CommitFileListBox/Item

---
+++ source/CommitLogView/UI/Units/CommitFileListBox.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace CommitLogView.UI.Units
+{
+    public class CommitFileListBox : RepoFileListBox
+    {
+        #region DefaultStyleKey
+
+        static CommitFileListBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CommitFileListBox), new FrameworkPropertyMetadata(typeof(CommitFileListBox)));
+        }
+        #endregion
+
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new CommitFileListItem();
+        }
+    }
+}
---
+++ source/CommitLogView/UI/Units/CommitFileListItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CommitLogView.UI.Units
+{
+    internal class CommitFileListItem : ListBoxItem
+    {
+        #region DefaultStyleKey
+
+        static CommitFileListItem()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CommitFileListItem), new FrameworkPropertyMetadata(typeof(CommitFileListItem)));
+        }
+        #endregion
+    }
+}
