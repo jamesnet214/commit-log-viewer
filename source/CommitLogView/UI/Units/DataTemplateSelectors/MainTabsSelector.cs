25d8486e684fe60dde0e33b918133feb5381f25c
james.lee <james.lee@devncore.org>
2021-12-17 23:40:57 +0900

    Update DataSelector name

--- source/CommitLogView/UI/Units/DataTemplateSelectors/MainTabsSelector.cs
+++ source/CommitLogView/UI/Units/DataTemplateSelectors/MainTabsSelector.cs
@@ -6,14 +6,14 @@ namespace CommitLogView.UI.Units.DataTemplateSelectors
 {
     public class MainTabsSelector : DataTemplateSelector
     {
-        public DataTemplate Repo { get; set; }
+        public DataTemplate Repos { get; set; }
         public DataTemplate Commit { get; set; }
 
         public override DataTemplate SelectTemplate(object item, DependencyObject container)
         {
             if (item is RepoViewModel)
             {
-                return Repo;
+                return Repos;
             }
             else if (item is CommitViewModel)
             {
